0ded0dc23270da2daef6448d22b29de890cf7764
Bruno Fukumori <brunofukumori@gmail.com>
2024-02-09 18:19:32 -0300

    :sparkles: feat: create polls route

--- src/http/server.ts
+++ src/http/server.ts
@@ -1,10 +1,23 @@
 import fastify from 'fastify';
 import { env } from '../env';
+import z from 'zod';
+import { PrismaClient } from '@prisma/client';
 
 const app = fastify();
+const prisma = new PrismaClient();
 
-app.post('/polls', (req) => {
-  console.log(req.body);
+app.post('/polls', async (req, reply) => {
+  const createPollBody = z.object({
+    title: z.string(),
+  });
+  const { title } = createPollBody.parse(req.body);
+  const { id } = await prisma.poll.create({
+    data: {
+      title,
+    },
+  });
+
+  return reply.status(201).send({ id });
 });
 
 app.listen({ port: env.PORT }).then(() => {
